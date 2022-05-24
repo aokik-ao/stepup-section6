56a1695abfeeea52d184361c7f685a97cc545297
aokik-ao <61224695+aokik-ao@users.noreply.github.com>
2022-05-24 13:25:31 +0900

    organism作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organism/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "青木",
+  image: "https://source.unsplash.com/q8-m4cp0BcQ",
+  email: "12345@example.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
